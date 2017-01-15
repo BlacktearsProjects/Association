3bd822672d47257a594146c861a12f5853a17697
Selim Ben Ammar <selim.benammar@telecom-paristech.fr>
2017-01-15 18:23:25 +0000

    Files of the authentication project
    The client and the server do not talk with each other. What is the issue ?

---
+++ server.js
@@ -0,0 +1,18 @@
+var http = require('http');
+
+
+callback = function(response) {
+  var str = '';
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+    console.log(str);
+  });
+}
+
+http.request("http://localhost:8080/", callback).end();
